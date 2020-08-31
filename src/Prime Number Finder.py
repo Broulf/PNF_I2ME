8600c30a51acebc8311a51ffd653491d6c66c9d8
Broulf <66291288+Broulf@users.noreply.github.com>
2020-08-31 09:18:37 -0500

    Add files via upload

--- src/Prime Number Finder.py
+++ src/Prime Number Finder.py
@@ -3,23 +3,20 @@
 # asks the user for an input (i.e. number)
 num = int(input("Enter a number: "))
 
-# checks if number is over 1
+# prime numbers are greater than 1
 if num > 1:
-    # check for factors of num
-    for i in range(2,num):
-        if (num % i) == 0:
-            # says in console if the number is not a prime number
-            print(num,"is not a prime number")
-            # breaks down the factors of the non prime number
-            print(i,"times",num//i,"is",num)
-            break
-    else:
-        # says in console that the number is prime
-        print(num,"is a prime number")
-
-# if input is less than
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
 # or equal to 1, it is not prime
 else:
-    print(num,"is not a prime number")
+   print(num,"is not a prime number")
 
 # https://www.programiz.com/python-programming/online-compiler/
